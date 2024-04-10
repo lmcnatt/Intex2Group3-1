f467ee9c97852d643716842183f725a44608006e
broncogooch <calebgooch2000@gmail.com>
2024-04-09 18:25:45 -0600

    merges

---
+++ Intex2/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Intex2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Intex2.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private IAdminRepository _repo;
+
+        public AdminController(IAdminRepository repoService)
+        {
+            _repo = repoService;
+        }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+        public IActionResult AddEditProducts()
+        {
+            return View();
+        }
+
+        public IActionResult Orders()
+        {
+            return View();
+        }
+    }
+}
