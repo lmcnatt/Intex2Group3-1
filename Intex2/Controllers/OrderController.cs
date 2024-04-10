dbd45e1ee6d7a2bb697d8e33ea66345187f9b408
Logan McNatt <logan.m.mcnatt@gmail.com>
2024-04-09 20:13:57 -0600

    Database update

--- Intex2/Controllers/OrderController.cs
+++ Intex2/Controllers/OrderController.cs
@@ -18,13 +18,38 @@ namespace Intex2.Controllers
             _repo = repoService;
             cart = cartService;
         }
+
+        public Customer Customer { get; set; }
+        public string CustomerId { get; set; }
+        public ICollection<Cart.CartLine> Lines { get; set;}
+        public string Date { get; set; }
+        public string DayOfWeek { get; set; }
+        public string Time { get; set; }
+        public decimal Amount { get; set; }
+        public string CountryOfTransaction { get; set; }
         public ViewResult Checkout() => View(new Order());
 
         [HttpPost]
         public IActionResult Checkout(Order order)
         {
+            // Check ModelState errors
+            foreach (var state in ModelState.Values)
+            {
+                foreach (var error in state.Errors)
+                {
+                    Console.WriteLine(error.ErrorMessage);
+                }
+            }
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             var customer = _repo.GetCustomer(userId);
+            order.Customer = customer;
+            order.CustomerId = customer.CustomerId;
+            order.Lines = cart.Lines.ToArray();
+            order.Date = datetime.ToString("dd/MM/yyyy");
+            order.DayOfWeek = datetime.ToString("ddd");
+            order.Time = datetime.ToString("HH");
+            order.Amount = cart.CalculateTotal();
+            order.CountryOfTransaction = customer.CountryOfResidence;
 
             if (cart.Lines.Count() == 0)
             {
@@ -33,12 +58,7 @@ namespace Intex2.Controllers
             }
             if (ModelState.IsValid)
             {
-                order.Lines = cart.Lines.ToArray();
-                order.Date = datetime.ToString("dd/MM/yyyy");
-                order.DayOfWeek = datetime.ToString("ddd");
-                order.Time = datetime.ToString("H");
-                order.Amount = cart.CalculateTotal();
-                order.CountryOfTransaction = customer.CountryOfResidence;
+                
                 _repo.SaveOrder(order);
                 cart.Clear();
                 return RedirectToPage("/Completed", new { orderId = order.OrderID });
